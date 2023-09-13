edd95eb8a437791dba0518388613ba8ad1a664cf
Saba Phruidze <sabaphruidze01@gmail.com>
2023-09-13 13:43:48 +0400

    there was a bit bug but corrected it

--- src/components/style/ContainerFlexRow.tsx
+++ src/components/style/ContainerFlexRow.tsx
@@ -1,17 +1,10 @@
 import styled from "styled-components";
 
-import React from "react";
-
-function ContainerFlexRow() {
-  const Container = styled.div`
-    width: 100%;
-    height: 750px;
-    display: flex;
-    flex-direction: row;
-    justify-content: center;
-    align-items: center;
-  `;
-  return <Container />;
-}
-
-export default ContainerFlexRow;
+export const ContainerFlexRow = styled.div`
+  width: 100%;
+  height: 714px;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+`;
--- src/components/style/Global.tsx
+++ src/components/style/Global.tsx
@@ -10,5 +10,3 @@ export const Global = createGlobalStyle`
     }
     
 `;
-
-export default Global;
